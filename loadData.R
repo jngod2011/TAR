f617e124e9a75ddff38f8e466464a5ff369b36a1
Michi Jeremias <michael.jeremias.home@gmail.com>
2015-10-14 23:13:24 +0200

    data

--- loadData.R
+++ loadData.R
@@ -1,20 +1,13 @@
 # load data
 
-m1 <- log(MEI$M1IDX_EA)-log(MEI$M1IDX_US)
-m3 <- log(MEI$M3IDX_EA)-log(MEI$M3IDX_US)
-y <- log(MEI$GDPIDX_US)-log(MEI$GDPIDX_EA)
-irlt <- MEI$IRLT_EA-MEI$IRLT_US
-e <- log(MEI$CCUS)
+m1 <- log(df.MEI_EA_US$M1IDX_EA)-log(df.MEI_EA_US$M1IDX_US)
+m3 <- log(df.MEI_EA_US$M3IDX_EA)-log(df.MEI_EA_US$M3IDX_US)
+y <- log(df.MEI_EA_US$GDPIDX_US)-log(df.MEI_EA_US$GDPIDX_EA)
+irlt <- df.MEI_EA_US$IRLT_EA-df.MEI_EA_US$IRLT_US
+e <- log(1/df.MEI_EA_US$CCUS)
 
 df.macro <- data.frame(e, m1, y, irlt)
-longRunResiduals <- as.numeric(summary(lm(e~., data=df.macro))$residuals)
+ve.longRunResiduals <- as.numeric(summary(lm(e~., data=df.macro))$residuals)
 
 rm(m1,m3,y,irlt,e)
-
-
-#old, probably useless, can't remember properly 
-de <- diff(e)
-
-# ecm 
-m.ecm <- summary(lm(diff(e)[2:length(diff(e))]~1+modelLongRunResiduals[2:length(modelLongRunResiduals)]+diff(e,lag = 2)))
 
